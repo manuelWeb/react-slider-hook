854582c547f6d0aeba5fa2c06db8e698323baed7
Manu2015 <devries.webdesigner@gmail.com>
2022-11-20 11:32:03 +0100

    **fix** add img src from Contentful
    To change if images becomes unreachable

--- src/images.js
+++ src/images.js
@@ -1,7 +1,7 @@
 const images = [
-  'https://medias-preprod.tempsl.fr/homeEvt/fr/E1-garden-decoration-FR-2021-1140w.jpg',
-  'https://medias-preprod.tempsl.fr/homeEvt/fr/E1-jours-fous-FR-2020-1140w.jpg',
-  'https://medias-preprod.tempsl.fr/homeEvt/fr/E1-meal-in-garden-FR-2021-1140w.jpg',
-  'https://medias-preprod.tempsl.fr/homeEvt/fr/E1-nettoyage-printemps-FR-2021-1140w.jpg',
+  'https://images.ctfassets.net/g44cg9jvafqp/1ErooXX5Brt7CtdVoA7ayk/f43fad2cbbfcbe6ddd8128b00f7f3002/TL-FR-black-week2_Desktop_slider1.jpg?fm=webp&w=1152',
+  'https://images.ctfassets.net/g44cg9jvafqp/7LGyB5v1cGwC30pTTybJfZ/55fc4d738e8a821c28ad21594b40ca02/TL-FR-BF-objets-feeriques_Desktop_slider2.jpg?fm=webp&w=1152',
+  // 'https://images.ctfassets.net/g44cg9jvafqp/64drWeCobzmsubiRImUckl/43cc198d12c00898a4ec50364115d16b/TL-FR-BF-petits_plats_Desktop_slider3.jpg?fm=webp&w=1152',
+  // 'https://images.ctfassets.net/g44cg9jvafqp/15Zff2JaGGOzEKITBf26eF/31aa6ddb1d9f42cad7580a0dcd0b684d/TL-FR-BF-Banner-grand_froid_desktop_slider4.jpg?fm=webp&w=1152',
 ]
 export default images;
